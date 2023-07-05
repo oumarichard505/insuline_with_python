5d1884dbb11e297d3adab1a4bd7f0403b5fb6107
Richard Ouma <richard.ouma@student.moringaschool.com>
2023-07-05 17:45:24 +0300

    Verification of the characters and Filterng the operaations

---
+++ filtered_proteins.py
@@ -0,0 +1,61 @@
+protein_sequence = """ORIGIN      
+        1 malwmrllpl lallalwgpd paaafvnqhl cgshlvealy lvcgergffy tpktrreaed
+       61 lqvgqvelgg gpgagslqpl alegslqkrg iveqcctsic slyqlenycn
+//"""
+
+# Remove unwanted characters and spaces by replacing with an empty string
+protein_sequence = protein_sequence.replace("ORIGIN", "").replace("1", "").replace("61", "").replace("//", "").replace(" ", "").replace("\n", "")
+
+print(protein_sequence)
+
+
+clean_protein_sequence = "malwmrllpllallalwgpdpaaafvnqhlcgshlvealylvcgergffytpktrreaedlqvgqvelgggpgagslqplalegslqkrgiveqcctsicslyqlenycn"
+
+print(len(clean_protein_sequence))
+
+
+
+
+# Save first 24 amino acids to file
+with open("insulin-seq-clean.txt", "w") as f:
+    f.write(clean_protein_sequence[:24])
+
+# Verify file has 24 characters
+with open("insulin-seq-clean.txt", "r") as f:
+    amino_acids = f.read()
+    print(len(amino_acids))
+
+
+
+
+# Save amino acids 25-54 to file
+with open("insulin-seq-clean.txt", "w") as f:
+    f.write(clean_protein_sequence[24:54])
+
+# Verify file has 30 characters
+with open("insulin-seq-clean.txt", "r") as f:
+    amino_acids2 = f.read()
+    print(len(amino_acids2))
+
+
+
+# Save amino acids 55-89 to file
+with open("insulin-seq-clean.txt", "w") as f:
+    f.write(clean_protein_sequence[54:89])
+
+# Verify file has 35 characters
+with open("insulin-seq-clean.txt", "r") as f:
+    amino_acids3 = f.read()
+    print(len(amino_acids3))
+    
+    
+
+# Save amino acids 90-110 to file
+with open("insulin-seq-clean.txt", "w") as f:
+    f.write(clean_protein_sequence[89:])
+
+# Verify file has 21 characters
+with open("insulin-seq-clean.txt", "r") as f:
+    amino_acids4 = f.read()
+    print(len(amino_acids4))
+
